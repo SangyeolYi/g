f3848d4853d3b3addc885ff75e9352309ead21ba
SangyeolYi <41245913+SangyeolYi@users.noreply.github.com>
2022-04-11 02:26:55 +0900

    1

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,31 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
+// let currentPopup: any = undefined;
 
-// Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+// // Waiting for the API to be ready
+// WA.onInit().then(() => {
+//     console.log('Scripting API ready');
+//     console.log('Player tags: ',WA.player.tags)
     
-    WA.room.onEnterLayer('clockZone').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
-    })
+//     WA.room.onEnterLayer('clockZone').subscribe(() => {
+//         const today = new Date();
+//         const time = today.getHours() + ":" + today.getMinutes();
+//         currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
+//     })
 
-    WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+//     WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
 
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+//     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+//     bootstrapExtra().then(() => {
+//         console.log('Scripting API Extra ready');
+//     }).catch(e => console.error(e));
     
-}).catch(e => console.error(e));
+// }).catch(e => console.error(e));
 
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+// function closePopUp(){
+//     if (currentPopup !== undefined) {
+//         currentPopup.close();
+//         currentPopup = undefined;
+//     }
+// }
